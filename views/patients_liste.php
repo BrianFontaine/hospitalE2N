0d83ccabcb3518ad5216f46f9e62404e3ee1710b
Brian Fontaine <brian02440@live.fr>
2020-08-21 17:23:09 +0200

    bug resolue pour ajax

--- views/patients_liste.php
+++ views/patients_liste.php
@@ -13,7 +13,6 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css"
         integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
     <link rel="stylesheet" href="../assets/css/style.css">
-
     <title>Liste des patients</title>
 </head>
 
@@ -133,5 +132,4 @@
             }
         })
     </script>
-
 </html>
